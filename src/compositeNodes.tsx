b963ca6fd50fadb0a73dd6299f1add54b1a604c3
styczynski <piotrsty1@gmail.com>
2019-06-24 19:07:20 +0200

    Update compositeNodes to match Omigost lastest internal version

--- src/compositeNodes.tsx
+++ src/compositeNodes.tsx
@@ -28,7 +28,7 @@ export abstract class CompositeNode<O, M extends NodeSchema> extends Node<NodeS,
     abstract getCompositeOutput(output: NodeO): NodeOutputValue<O>;
 
     renderComposite(context: FormContext, children: ChildrenMap<React.ReactNode>): React.ReactNode {
-        return Object.values(children);
+        return Object.keys(children).map(key => children[key]);
     }
 
     resolveInitialState() {
@@ -39,10 +39,29 @@ export abstract class CompositeNode<O, M extends NodeSchema> extends Node<NodeS,
         return initialState;
     }
 
-    getRawOutput() {
+    getValueMapFromValue(value: NodeOutputValue<O>): NodeOutputValue<O> {
+        return { ...value };
+    }
+
+    setValue(value: NodeOutputValue<O>) {
+        if (this.getSchema().formatInput) {
+            value = this.getSchema().formatInput(value);
+        }
+        value = this.getValueMapFromValue(value);
+
+        Object.keys(this.getChildrenMapFromSchema()).forEach(key => {
+            if (this.findChild(key)) {
+                this.findChild(key).setValue(value[key]);
+            }
+        });
+    }
+
+    getRawOutput(options) {
         const output = {};
         Object.keys(this.getChildrenMapFromSchema()).forEach(key => {
-            output[key] = this.findChild(key).getOutput();
+            if (this.findChild(key) && this.findChild(key).isOutputAvailable()) {
+                output[key] = this.findChild(key).getOutput(options);
+            }
         });
         return this.getCompositeOutput(output);
     }
@@ -67,7 +86,9 @@ export abstract class CompositeNode<O, M extends NodeSchema> extends Node<NodeS,
         const childrenMap: ChildrenMap<React.ReactNode> = {};
 
         Object.keys(this.getChildrenMapFromSchema()).forEach((key: string, index: number) => {
-            childrenMap[key] = this.findChild(key).render(context);
+            if (this.findChild(key)) {
+                childrenMap[key] = this.findChild(key).render(context);
+            }
         });
 
         return this.renderComposite(context, childrenMap);
